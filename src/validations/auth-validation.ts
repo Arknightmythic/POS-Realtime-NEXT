13e59b81724e58fe189da7c417aaca03a4a3d1aa
CLP <a2017@2017s-MacBook-Pro.local>
2025-07-23 08:40:17 +0700

    already finish login

---
+++ src/validations/auth-validation.ts
@@ -0,0 +1,12 @@
+import z from "zod";
+
+export const loginSchemaForm = z.object({
+  email: z
+    .string()
+    .min(1, "email is required")
+    .email("please enter a valid email"),
+  password: z.string().min(1, "password is required"),
+});
+
+
+export type LoginForm = z.infer<typeof loginSchemaForm>;
